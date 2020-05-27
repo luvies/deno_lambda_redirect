fd92f2b3de4c0252527d8264f8e2fd2e91225c66
Luke Davies <lkdvs3@gmail.com>
2020-05-27 11:46:55 +0100

    Initial commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+import { APIGatewayProxyHandler } from "https://deno.land/x/lambda/types.d.ts";
+
+export const handler: APIGatewayProxyHandler = async () => {
+  const location = Deno.env.get("REDIRECT_URL");
+
+  if (location) {
+    return {
+      statusCode: 302,
+      headers: {
+        location,
+      },
+      body: "",
+    };
+  } else {
+    return {
+      statusCode: 500,
+      body: "Location not set",
+    };
+  }
+};
