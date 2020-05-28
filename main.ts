927982d7af750db26fa3f1e69dc9ba9841bba570
Luke Davies <lkdvs3@gmail.com>
2020-05-28 16:29:46 +0100

    Adjusted lambdas

--- main.ts
+++ main.ts
@@ -3,18 +3,14 @@ import { APIGatewayProxyHandler } from "https://deno.land/x/lambda/types.d.ts";
 const location = Deno.env.get("REDIRECT_URI");
 
 export const handler: APIGatewayProxyHandler = location
-  ? (async () => {
-    return {
-      statusCode: 302,
-      headers: {
-        location,
-      },
-      body: "",
-    };
-  })
-  : (async () => {
-    return {
-      statusCode: 500,
-      body: "Location not set",
-    };
-  });
+  ? (async () => ({
+    statusCode: 302,
+    headers: {
+      location,
+    },
+    body: "",
+  }))
+  : (async () => ({
+    statusCode: 500,
+    body: "Location not set",
+  }));
